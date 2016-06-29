3e3421e3f00981f24b947502da3b2e8d2d73fcdc
glorynguyen <nguyenbavinhcntt@gmail.com>
2016-06-29 22:45:37 +0700

    customize manage screen

--- js/table_googlesheet.js
+++ js/table_googlesheet.js
@@ -1,147 +1,152 @@
-const ALERT_DEFAULT_VALUE = "Đang tải dữ liệu...";
+const ALERT_DEFAULT_VALUE = "Đã tải dữ liệu thành công\nĐang hiển thị dữ liệu...";
 const ID_TABLE = "list";
 const SEPERATOR_SPLIT = ",";
 const SUM_ID = 'tongValue';
 function printElements(data) {
-    var alertField = document.getElementsByClassName("alert alert-success")[0],
+    try {
+        var alertField = document.getElementsByClassName("alert alert-success")[0],
         arr = (data) ? data.split(String.fromCharCode(10)) : null, divRowTable,
         length = (arr) ? arr[0].split(SEPERATOR_SPLIT).length : 0,
         divMd12, tbl, thead, tr, td, i, j, th, tbdy;
 
-    /*Update alert field value*/
-    if (alertField) {
-        alertField.setAttribute('class', 'alert alert-info');
-        alertField.getElementsByTagName('strong')[0].innerHTML = ALERT_DEFAULT_VALUE;
-    }
+        /*Update alert field value*/
+        /*if (alertField) {
+            alertField.setAttribute('class', 'alert alert-info');
+            alertField.getElementsByTagName('strong')[0].innerHTML = ALERT_DEFAULT_VALUE;
+        }*/
 
-    /*Create div bootstrap for table*/
-    divRowTable = document.createElement('div');
-    divRowTable.setAttribute('class', 'row');
-    divMd12 = document.createElement('div');
-    divMd12.setAttribute('class', 'col-ms-12');
+        /*Create div bootstrap for table*/
+        divRowTable = document.createElement('div');
+        divRowTable.setAttribute('class', 'row');
+        divMd12 = document.createElement('div');
+        divMd12.setAttribute('class', 'col-ms-12');
 
-    /*Create table*/
-    tbl = document.createElement('table');
-    tbl.setAttribute('id', ID_TABLE);
-    tbl.setAttribute('class', 'table table-responsive');
-    tbl.style.width = '100%';
-    tbl.setAttribute('border', '1');
-    thead = document.createElement('thead');
-    tr = document.createElement('tr');
-    tr.setAttribute('class', 'success');
+        /*Create table*/
+        tbl = document.createElement('table');
+        tbl.setAttribute('id', ID_TABLE);
+        tbl.setAttribute('class', 'table table-responsive');
+        tbl.style.width = '100%';
+        tbl.setAttribute('border', '1');
+        thead = document.createElement('thead');
+        tr = document.createElement('tr');
+        tr.setAttribute('class', 'success');
 
-    /*Create table header then append to the created table*/
-    for (j = 0; j < length; j++) {
-        th = document.createElement('th');
-        th.appendChild(document.createTextNode(buldStringHeader(arr[0].split(SEPERATOR_SPLIT)[j])));
-        tr.appendChild(th);
-    }
-    thead.appendChild(tr);
+        /*Create table header then append to the created table*/
+        for (j = 0; j < length; j++) {
+            th = document.createElement('th');
+            th.appendChild(document.createTextNode(buldStringHeader(arr[0].split(SEPERATOR_SPLIT)[j])));
+            tr.appendChild(th);
+        }
+        thead.appendChild(tr);
 
-    /*Create the table body*/
-    var dateFilter = document.getElementById('dateFilter').value;
-    tbdy = document.createElement('tbody');
-    for (i = 1; i < arr.length; i++) {
-        var date = revertDate(arr[i].split(',')[0].split(" ")[0]);
-        
-        if (dateFilter) {
-            if (date === dateFilter) {
-                
-            }    
-        } else {
-            tr = document.createElement('tr');
-            for (j = 0; j < length; j++) {
-                td = document.createElement('td');
-                td.onclick = function(e) {
-                    //Check is filtered
-                    var bd = this.parentNode.parentNode, ul = document.createElement('ul'),
-                        li = document.createElement('li');
-                    if (bd.getAttribute('filtered') != "true") {
-                        // check is datetime column
-                        if (this.parentNode.children[0] == this) {
-                            // Check has menu yet
-                            if (this.children.length == 0) {
-                                // Create menu
-                                li.textContent = 'Xem dữ liệu ngày: ' + this.innerHTML.split(" ")[0];
-                                li.setAttribute('style', 'color:#E48BE4; cursor: pointer;');
-                                li.onmouseover = function() {
-                                    this.setAttribute('style', 'color: #6B066B; cursor: pointer;');
+        /*Create the table body*/
+        var dateFilter = document.getElementById('dateFilter').value;
+        tbdy = document.createElement('tbody');
+        for (i = 1; i < arr.length; i++) {
+            var date = revertDate(arr[i].split(',')[0].split(" ")[0]);
+            
+            if (dateFilter) {
+
+                if (new Date(date).getTime() === new Date(dateFilter).getTime()) {
+                    debugger
+                }    
+            } else {
+                tr = document.createElement('tr');
+                for (j = 0; j < length; j++) {
+                    td = document.createElement('td');
+                    td.onclick = function(e) {
+                        //Check is filtered
+                        var bd = this.parentNode.parentNode, ul = document.createElement('ul'),
+                            li = document.createElement('li');
+                        if (bd.getAttribute('filtered') != "true") {
+                            // check is datetime column
+                            if (this.parentNode.children[0] == this) {
+                                // Check has menu yet
+                                if (this.children.length == 0) {
+                                    // Create menu
+                                    li.textContent = 'Xem dữ liệu ngày: ' + this.innerHTML.split(" ")[0];
+                                    li.setAttribute('style', 'color:#E48BE4; cursor: pointer;');
+                                    li.onmouseover = function() {
+                                        this.setAttribute('style', 'color: #6B066B; cursor: pointer;');
+                                    }
+                                    li.onmouseleave = function() {
+                                        this.setAttribute('style', 'color: #E48BE4; cursor: pointer;');
+                                    }
+                                    li.onclick = onClickTableRow;
+                                    ul.appendChild(li);
+                                    this.appendChild(ul);
+                                } else {
+                                    this.removeChild(this.children[0]);
                                 }
-                                li.onmouseleave = function() {
-                                    this.setAttribute('style', 'color: #E48BE4; cursor: pointer;');
-                                }
-                                li.onclick = onClickTableRow;
-                                ul.appendChild(li);
-                                this.appendChild(ul);
-                            } else {
-                                this.removeChild(this.children[0]);
                             }
                         }
+                    };
+                    if (j == length - 1) {
+                        td.appendChild(document.createTextNode(addCommas(arr[i].split(",")[j])));
+                    } else {
+                        td.appendChild(document.createTextNode(arr[i].split(",")[j]));
                     }
-                };
-                if (j == length - 1) {
-                    td.appendChild(document.createTextNode(addCommas(arr[i].split(",")[j])));
-                } else {
-                    td.appendChild(document.createTextNode(arr[i].split(",")[j]));
+                    tr.appendChild(td)
                 }
-                tr.appendChild(td)
+                tbdy.appendChild(tr);
             }
-            tbdy.appendChild(tr);
+        }
+        tbl.appendChild(thead);
+        tbl.appendChild(tbdy);
+
+        divMd12.appendChild(tbl);
+        divRowTable.appendChild(divMd12);
+
+
+        //Add button add 
+        var rowDiv = document.createElement('div');
+        rowDiv.setAttribute('class', 'row');
+
+        var newDivAdd = document.createElement('div');
+        newDivAdd.setAttribute('id', "add");
+
+        var newRowBtn = document.createElement('div');
+        newRowBtn.setAttribute('class', "row");
+        newRowBtn.setAttribute('id', "rowAddBtn");
+
+        var addButton = document.createElement('button');
+        addButton.setAttribute('id', "btnAdd");
+        addButton.innerHTML = "Thêm";
+        addButton.setAttribute('onclick', "add()");
+        addButton.setAttribute('class', 'btn btn-info');
+
+        newRowBtn.appendChild(addButton);
+
+        newDivAdd.appendChild(newRowBtn);
+
+        rowDiv.appendChild(newDivAdd);
+        var divAuto = document.getElementById('generateAuto');
+        divAuto.appendChild(divRowTable);
+        // Create lable for tongTien
+        var newDivTong = document.createElement('div');
+        newDivTong.setAttribute('class', 'row');
+        var h1Tong = document.createElement('h1');
+        var lbTong = document.createElement('span');
+        lbTong.setAttribute('class', 'label label-success');
+        lbTong.innerHTML = 'Tổng: ';
+        var lbTongValue = document.createElement('span');
+        lbTongValue.setAttribute('class', 'label label-danger');
+        lbTongValue.setAttribute('id', SUM_ID);
+        lbTongValue.setAttribute('style', 'position: absolute;right: 7px;');
+        lbTongValue.innerHTML = this.tongTien();
+        h1Tong.appendChild(lbTong);
+        h1Tong.appendChild(lbTongValue);
+        newDivTong.appendChild(h1Tong);
+        divAuto.appendChild(newDivTong);
+        divAuto.appendChild(newDivAdd);
+        if (briefcase.resolve) {
+            briefcase.resolve();
+        }
+    } catch (ex) {
+        if (briefcase.reject) {
+            briefcase.reject(ex);
         }
     }
-    tbl.appendChild(thead);
-    tbl.appendChild(tbdy);
-
-    divMd12.appendChild(tbl);
-    divRowTable.appendChild(divMd12);
-
-
-    //Add button add 
-    var rowDiv = document.createElement('div');
-    rowDiv.setAttribute('class', 'row');
-
-    var newDivAdd = document.createElement('div');
-    newDivAdd.setAttribute('id', "add");
-
-    var newRowBtn = document.createElement('div');
-    newRowBtn.setAttribute('class', "row");
-    newRowBtn.setAttribute('id', "rowAddBtn");
-
-    var addButton = document.createElement('button');
-    addButton.setAttribute('id', "btnAdd");
-    addButton.innerHTML = "Thêm";
-    addButton.setAttribute('onclick', "add()");
-    addButton.setAttribute('class', 'btn btn-info');
-
-    newRowBtn.appendChild(addButton);
-
-    newDivAdd.appendChild(newRowBtn);
-
-    rowDiv.appendChild(newDivAdd);
-    var divAuto = document.getElementById('generateAuto');
-    divAuto.appendChild(divRowTable);
-    // Create lable for tongTien
-    var newDivTong = document.createElement('div');
-    newDivTong.setAttribute('class', 'row');
-    var h1Tong = document.createElement('h1');
-    var lbTong = document.createElement('span');
-    lbTong.setAttribute('class', 'label label-success');
-    lbTong.innerHTML = 'Tổng: ';
-    var lbTongValue = document.createElement('span');
-    lbTongValue.setAttribute('class', 'label label-danger');
-    lbTongValue.setAttribute('id', SUM_ID);
-    lbTongValue.setAttribute('style', 'position: absolute;right: 7px;');
-    lbTongValue.innerHTML = this.tongTien();
-    h1Tong.appendChild(lbTong);
-    h1Tong.appendChild(lbTongValue);
-    newDivTong.appendChild(h1Tong);
-    divAuto.appendChild(newDivTong);
-    divAuto.appendChild(newDivAdd);
-
-    var alertField = document.getElementsByClassName("alert alert-info")[0];
-    alertField.setAttribute('class', 'alert alert-success');
-    alertField.getElementsByTagName('strong')[0].innerHTML = "Tải dữ liệu thành công";
-
 }
 
 function revertDate(date) {
@@ -194,7 +199,9 @@ function capitalizeFirstLetter(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
-function showTable(documentId) {
+function showTable(documentId, resolve, reject) {
+    briefcase.resolve = resolve;
+    briefcase.reject = reject;
     briefcase.getCSV({
         id: documentId
     }, printElements);
